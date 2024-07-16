185fa0f6e0d9b1aaf434cf7e7c9258e7c70b7419
Eduardo Rigon <70084379+edukatiau@users.noreply.github.com>
2024-07-16 12:43:12 -0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -36,3 +36,43 @@ SELECT Playlists.nome, COUNT(PlaylistMusicas.musica_id) AS num_musicas
 FROM Playlists
 LEFT JOIN PlaylistMusicas ON Playlists.id = PlaylistMusicas.playlist_id
 GROUP BY Playlists.id;
+
+-- 10) Calcula a media de notas de uma musica especifica
+SELECT AVG(nota) AS media_avaliacao FROM Avaliacoes WHERE musica_id = 1;
+
+-- 11) Exibe os 5 ultimos artistas adicionados no banco
+SELECT * FROM Artistas ORDER BY id DESC LIMIT 5;
+
+-- 12) Exibe todas as músicas adicionadas a uma playlist específica em ordem decrescente de data de adição
+SELECT Musicas.titulo AS musica, PlaylistMusicas.data_adicao AS data_adicao
+FROM PlaylistMusicas
+JOIN Musicas ON PlaylistMusicas.musica_id = Musicas.id
+WHERE PlaylistMusicas.playlist_id = 1
+ORDER BY PlaylistMusicas.data_adicao DESC;
+
+-- 13) Exibe o total de musicas por album
+SELECT Albuns.titulo AS album,COUNT(Musicas.id) AS total_musicas
+FROM Musicas
+JOIN Albuns ON Musicas.album_id = Albuns.id
+GROUP BY Albuns.titulo;
+
+-- 14. Listar o total de avaliações por música
+SELECT Musicas.titulo AS musica, COUNT(Avaliacoes.id) AS total_avaliacoes
+FROM Avaliacoes
+JOIN Musicas ON Avaliacoes.musica_id = Musicas.id
+GROUP BY Musicas.titulo;
+
+-- 15) Exibe todas as musicas com nota 5
+SELECT Musicas.titulo 
+FROM Musicas
+JOIN Avaliacoes ON Musicas.id = Avaliacoes.musica_id
+WHERE Avaliacoes.nota = 5;
+
+-- 16) Exibe todas as músicas cujo título iniciam a palavra 'He'
+SELECT * FROM Musicas WHERE titulo LIKE 'He%';
+
+-- 17) Exibe todos os usuários e a quantidade de playlists criadas por cada um
+SELECT Usuarios.nome AS usuario, COUNT(Playlists.id) AS total_playlists
+FROM Playlists
+JOIN Usuarios ON Playlists.usuario_id = Usuarios.id
+GROUP BY Usuarios.nome;
